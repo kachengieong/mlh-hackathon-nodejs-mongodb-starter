ccc680ffe73fb549465d1d09df752f12893a1d3c
Imma-Olly <oluomajoan@gmail.com>
2020-07-22 00:18:50 -0400

    created user registration and login.

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
-dotenv.config({ path: ".env" });
+// dotenv.config({ path: ".env" });
 
 module.exports = {
   port: process.env.PORT || 5000,
@@ -9,3 +9,37 @@ module.exports = {
   githubClientId: process.env.GITHUB_CLIENT_ID || "",
   githubClientSecret: process.env.GITHUB_CLIENT_SECRET || ""
 };
+
+var http = require ('http');         // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring = 
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb+srv://cluster0.j52nh.mongodb.net/mlh-hackathon-nodejs-starter --username Imma ';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+// const mongoose = require ('mongoose');
+
+// mongoose.connect('mongodb://localhost/27010/mlh-node-js-starter');
+
+// mongoose.connection.once('open', function(){
+//   console.log('connection has been made');
+// }).on('error', function(error){
+//   console.log('connection error:', error);
+// });
